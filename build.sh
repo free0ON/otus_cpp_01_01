7528f84444543f69a1c8a880ed50f5f7687348bd
free0ON <8333184@gmail.com>
2022-10-03 18:00:42 +0300

    Add description homepage_url  min_version

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 mkdir build
 cd build
-cmake .. -DGOOGLE_TEST=OFF -DCATCH2_TEST=OFF -DPATCH_VERSION=10
+cmake .. -DPRJ_CMAKE_MIN_VERSION="3.3" -DPRJ_NAME=helloworld \
+-DPRJ_VERSION_MAJOR=0 -DPRJ_VERSION_MINOR=0 -DPRJ_VERSION_PATCH=77 \
+-DPRJ_DESCRIPTION="Test program" \
+-DPRJ_HOMEPAGE_URL="https://github.com/free0ON/otus_cpp_01_01" \
+-DEMAIL_CONTACT=dmitrii.lozovoi@yandex.ru \
+-DGOOGLE_TEST=ON -DBOOST_TEST=ON -DCATCH2_TEST=ON
 cmake --build . 
 cmake --build . --target test
 cmake --build . --target package
