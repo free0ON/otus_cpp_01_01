5035da8fa9976bc9468b6ee0739b60e0e23738ec
free0ON <8333184@gmail.com>
2022-10-04 12:42:19 +0300

    GTEST_HAS_PTHREAD

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ cmake .. -DPRJ_CMAKE_MIN_VERSION="3.3" -DPRJ_NAME=helloworld \
 -DPRJ_DESCRIPTION="Test program" \
 -DPRJ_HOMEPAGE_URL="https://github.com/free0ON/otus_cpp_01_01" \
 -DEMAIL_CONTACT=dmitrii.lozovoi@yandex.ru \
--DGOOGLE_TEST=ON -DBOOST_TEST=ON -DCATCH2_TEST=ON
+-DGOOGLE_TEST=ON -DBOOST_TEST=OFF -DCATCH2_TEST=OFF
 cmake --build . 
-cmake --build . --target test
+cmake --build . --target test # -DGTEST_HAS_PTHREAD=0
 cmake --build . --target package
 helloworld_cli
